f23ef4428cb458381db09de0385f490e7f8e46d8
ductaip <dtai858@gmail.com>
2025-01-21 22:49:08 +0700

    style component: complete home page and built button component

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from 'react-native'
 import React from 'react'
+import Button from '@/components/Button'
+import { router } from 'expo-router'
 
 const AuthLayout = () => {
   return (
     <View>
-      <Text>AuthLayout</Text>
+      <Text className='bg-red-500'>AuthLayout...</Text> 
     </View>
   )
 }
