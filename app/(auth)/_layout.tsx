c6db39fbb396a764462f0a42ee9ed8b9987d4634
ductaip <dtai858@gmail.com>
2025-01-24 00:02:54 +0700

    auth styles: handle UI sign-in page and config layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,28 @@
 import { Text, View } from 'react-native'
 import React from 'react'
 import Button from '@/components/Button'
-import { router } from 'expo-router'
+import { router, Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text className='bg-red-500'>AuthLayout...</Text> 
-    </View>
+    <React.Fragment>
+      <Stack>
+        <Stack.Screen 
+          name='sign-in'
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen 
+          name='sign-up'
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="dark" />
+    </React.Fragment>
   )
 }
 
--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,32 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '@/constants'
+import Form from '@/components/Form'
+import CustomButton from '@/components/Button'
+import { router } from 'expo-router'
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center h-full px-4 my-6'>
+          <Image 
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[115px] h-[35px]'
+          />
+
+          <Text className='text-2xl text-white text-semibold mt-10 font-pop-sb'>Login to App</Text>
+ 
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
