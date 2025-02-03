c7eb89163d674cd238e2abf71f7f9de9a678041b
ductaip <dtai858@gmail.com>
2025-02-03 17:04:59 +0700

    config all function and info from appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    platform: 'com.native.application',
+    projectId: '67a08aab002334b25daa',
+    databaseId: '67a08e51003e628f789f',
+    userCollectionId: '67a08ebf001f71d73f57',
+    videoCollectionId: '67a08f15002bac4465cb',
+    storageId: '67a0932c0005c69781d0'
+}
